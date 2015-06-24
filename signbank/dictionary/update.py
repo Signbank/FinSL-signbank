cbe5c690d0e56457c1340b45a1d920dc2728b256
Henri Nieminen <henri.nieminen@gmail.com>
2015-06-24 11:49:09 +0300

    Added an if to see that idgloss is unique in Add Gloss. Related to issue #34.

--- signbank/dictionary/update.py
+++ signbank/dictionary/update.py
@@ -32,6 +32,12 @@ def add_gloss(request):
             # Translators: Warning message in Add Gloss
             return render_to_response('dictionary/warning.html', {'warning': _('Gloss field is empty')},
                                       context_instance=RequestContext(request))
+                # Check that the idgloss is unique
+        if len(Gloss.objects.filter(idgloss=request.POST['idgloss'])) != 0:
+            # Translators: warning message in Add Gloss
+            return render_to_response('dictionary/warning.html', {'warning': _('Gloss not unique')},
+                                      context_instance=RequestContext(request))
+
         # Check that the JKL annotation gloss is unique
         if len(Gloss.objects.filter(annotation_idgloss_jkl=request.POST['annotation_idgloss_jkl'])) != 0:
             # Translators: warning message in Add Gloss
