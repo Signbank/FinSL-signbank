0342dcc7f3cbd1285ab75a5ad6c03f5eee12dda8
Henri Nieminen <henri.nieminen@gmail.com>
2015-07-02 11:32:20 +0300

    Django docs say that: Built-in template context processors were moved from django.core.context_processors to django.template.context_processors in Django 1.8. Done that.

--- signbank/settings/base.py
+++ signbank/settings/base.py
@@ -88,12 +88,12 @@ MIDDLEWARE_CLASSES = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.static",
-    "django.core.context_processors.tz",
-    "django.core.context_processors.request",
+    "django.template.context_processors.debug",
+    "django.template.context_processors.i18n",
+    "django.template.context_processors.media",
+    "django.template.context_processors.static",
+    "django.template.context_processors.tz",
+    "django.template.context_processors.request",
     "django.contrib.auth.context_processors.auth",
     "django.contrib.messages.context_processors.messages",
     "signbank.pages.context_processors.menu",
