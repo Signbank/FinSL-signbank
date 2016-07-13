60030ad147edb818e92f68e22c6411092aac0f11
heilniem <henri.nieminen@gmail.com>
2016-07-13 19:24:58 +0300

    Removed views but forgot to remove them from video/urls.py

--- signbank/video/urls.py
+++ signbank/video/urls.py
@@ -4,10 +4,6 @@ urlpatterns = [
     url(r'^video/(?P<videoid>\d+)$',
      'signbank.video.views.video'),
     url(r'^upload/', 'signbank.video.views.addvideo'),
-    url(r'^delete/(?P<videoid>\d+)$',
-     'signbank.video.views.deletevideo'),
     url(r'^poster/(?P<videoid>\d+)$',
      'signbank.video.views.poster'),
-    url(r'^iframe/(?P<videoid>\d+)$',
-     'signbank.video.views.iframe'),
 ]
