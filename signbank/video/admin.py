94563c3d61a5dbc760291f23eb50f4e7bee440a1
heilniem <henri.nieminen@gmail.com>
2016-07-13 14:28:51 +0300

    Made GlossVideo's admin page better. Filtering based on dataset is possible, showing more data: gloss, videofile, primary key, version.

--- signbank/video/admin.py
+++ signbank/video/admin.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
-
 from models import GlossVideo
 
 
-# admin.site.register(Video)
-
-
 class GlossVideoAdmin(admin.ModelAdmin):
-    search_fields = ['^gloss']
-
+    search_fields = ['^gloss__idgloss']
+    list_display = ['gloss', 'videofile', 'pk', 'version']
+    list_filter = ['gloss__dataset']
 
 admin.site.register(GlossVideo, GlossVideoAdmin)
