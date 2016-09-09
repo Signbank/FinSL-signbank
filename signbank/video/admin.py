c075e8071a13136a5283903005b331e94db995ba
heilniem <henri.nieminen@gmail.com>
2016-09-09 10:59:19 +0300

    Added dataset to GlossVideo admins list_display

--- signbank/video/admin.py
+++ signbank/video/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from models import GlossVideo
+from django.utils.translation import ugettext_lazy as _
 
 
 class GlossVideoAdmin(admin.ModelAdmin):
-    search_fields = ['^gloss__idgloss']
-    list_display = ['gloss', 'videofile', 'pk', 'version']
-    list_filter = ['gloss__dataset']
+    search_fields = ('^gloss__idgloss',)
+    list_display = ('gloss', 'dataset', 'videofile', 'pk', 'version')
+    list_filter = ('gloss__dataset',)
+
+    def dataset(self, obj):
+        """Getting the dataset for the glossvideos gloss"""
+        return obj.gloss.dataset
+    dataset.short_description = _('dataset')
+
 
 admin.site.register(GlossVideo, GlossVideoAdmin)
