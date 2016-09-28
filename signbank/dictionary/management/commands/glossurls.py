e3b0825285943e3b2be81bf41555cad1daa605a6
heilniem <henri.nieminen@gmail.com>
2016-09-28 15:59:05 +0300

    Fixed glossurls command.

--- signbank/dictionary/management/commands/glossurls.py
+++ signbank/dictionary/management/commands/glossurls.py
@@ -1,16 +1,13 @@
-"""Convert a video file to flv"""
-
+"""This command lists all Glosses and a list of each Glosses GlossVideos"""
 from django.core.management.base import BaseCommand
-
 from signbank.dictionary.models import Gloss
+from signbank.video.models import GlossVideo
 
 
 class Command(BaseCommand):
-
     help = 'generate a list of gloss IDs and their video URLs'
     args = ''
 
     def handle(self, *args, **options):
-
         for gloss in Gloss.objects.all():
-            print gloss.id, gloss.get_video_url() # TODO: This will not work, get_video_url is removed from gloss
+            print gloss.id, GlossVideo.get_glosses_videos(gloss)
