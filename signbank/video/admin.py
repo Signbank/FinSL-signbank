858d48fdc6807d60265695e2e505bbf5dd69ff9f
heilniem <henri.nieminen@gmail.com>
2017-04-04 12:08:13 +0300

    Added videofile to be searchable in GlossVideoAdmin

--- signbank/video/admin.py
+++ signbank/video/admin.py
@@ -42,7 +42,7 @@ class HasPosterFilter(admin.SimpleListFilter):
 
 class GlossVideoAdmin(admin.ModelAdmin):
     fields = ('title', 'videofile', 'posterfile', 'dataset', 'gloss', 'version')
-    search_fields = ('^gloss__idgloss',)
+    search_fields = ('^gloss__idgloss', 'videofile')
     list_display = ('gloss', 'dataset_video', 'title', 'videofile', 'posterfile', 'pk', 'version')
     list_filter = ('gloss__dataset', HasGlossFilter, 'dataset', HasPosterFilter,)
     form = GlossVideoAdminForm
