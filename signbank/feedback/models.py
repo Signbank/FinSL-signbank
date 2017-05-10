5124bc50227f117b9894846106c203b70bdacc0e
heilniem <henri.nieminen@gmail.com>
2017-05-10 20:15:55 +0300

    Removing 'feedback' app. Part 3, done.

--- signbank/feedback/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import models as authmodels
-from django.utils.translation import ugettext_lazy as _
-
-#from signbank.dictionary.models import *
-#import string
-# from signbank.dictionary.models import Gloss
-
-# models to represent the feedback from users in the site
-
