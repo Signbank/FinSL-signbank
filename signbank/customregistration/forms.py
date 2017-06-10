bc74a520d938b3206dd978ffdb1288aa1a91c8c5
heilniem <henri.nieminen@gmail.com>
2017-06-10 14:30:47 +0300

    Also added email into the registration form. For some reason these fields did not get bound, but password fields do.

--- signbank/customregistration/forms.py
+++ signbank/customregistration/forms.py
@@ -4,7 +4,7 @@ from registration.forms import RegistrationForm
 class CustomUserForm(RegistrationForm):
     """Custom form to require users to add their first and last names when registering."""
     class Meta(RegistrationForm.Meta):
-        fields = ['username', 'first_name', 'last_name']
+        fields = ['username', 'first_name', 'last_name', 'email']
 
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
