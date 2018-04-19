475be23ec1bc1c5c220bd59e3ab22c57f16bc360
henrinie <henri.nieminen@gmail.com>
2018-04-19 19:27:26 +0300

    Revert "Merge branch 'custom_user'"
    This reverts commit 815408f8c03847c4a8cbd34894d437b60a3a9517, reversing
    changes made to 29577cb3c3da3ad2a7003aed4091903d7b960664.

--- signbank/customregistration/forms.py
+++ signbank/customregistration/forms.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from registration.forms import RegistrationForm, RegistrationFormTermsOfService
+from registration.forms import RegistrationForm
 
 
-class CustomUserForm(RegistrationFormTermsOfService):
+class CustomUserForm(RegistrationForm):
     """Custom form to require users to add their first and last names when registering."""
-    class Meta(RegistrationFormTermsOfService.Meta):
-        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'tos']
+    class Meta(RegistrationForm.Meta):
+        fields = ['username', 'first_name', 'last_name', 'email']
 
     def __init__(self, *args, **kwargs):
-        super(RegistrationFormTermsOfService, self).__init__(*args, **kwargs)
+        super(RegistrationForm, self).__init__(*args, **kwargs)
         self.fields['first_name'].required = True
         self.fields['last_name'].required = True
