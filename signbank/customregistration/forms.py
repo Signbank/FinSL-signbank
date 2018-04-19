3e3fe1dcb93eac0713af8ca28db48c892e0a4a4c
henrinie <henri.nieminen@gmail.com>
2018-04-19 19:54:05 +0300

    Registration form to TOS form.

--- signbank/customregistration/forms.py
+++ signbank/customregistration/forms.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from registration.forms import RegistrationForm
+from django.utils.translation import ugettext_lazy as _
+
+from registration.forms import RegistrationFormTermsOfService
 
 
-class CustomUserForm(RegistrationForm):
-    """Custom form to require users to add their first and last names when registering."""
-    class Meta(RegistrationForm.Meta):
-        fields = ['username', 'first_name', 'last_name', 'email']
+class CustomUserForm(RegistrationFormTermsOfService):
+    """Custom form to require users to add their first and last names when registering,
+    and to allow translation of tos."""
+    class Meta(RegistrationFormTermsOfService.Meta):
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'tos']
 
     def __init__(self, *args, **kwargs):
-        super(RegistrationForm, self).__init__(*args, **kwargs)
+        super(RegistrationFormTermsOfService, self).__init__(*args, **kwargs)
         self.fields['first_name'].required = True
         self.fields['last_name'].required = True
+        self.fields['tos'].label = _('I have read and agree to the Terms of Service')
