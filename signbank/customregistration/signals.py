98790a768690920721a3a252e7853779ef194ba9
henrinie <henri.nieminen@gmail.com>
2018-06-08 13:02:54 +0300

    On user registration, send notification to superusers, remove group "Super admin"

--- signbank/customregistration/signals.py
+++ signbank/customregistration/signals.py
@@ -10,7 +10,7 @@ from notifications.signals import notify
 def notify_on_account_creation(sender, **kwargs):
     """When a user creates an account, send a notification to Users in 'Super admin' group (with is_staff=True)."""
     acc = kwargs["user"]
-    notify.send(sender=acc, recipient=User.objects.filter(is_staff=True, groups__name="Super admin"),
+    notify.send(sender=acc, recipient=User.objects.filter(is_staff=True, is_superuser=True),
                 verb=_("created an account."), public=False,
                 description="{acc.first_name} {acc.last_name} {created}: '{acc}'. {emailtxt}: {acc.email}".format(
                     acc=acc, created=_("created an account"), emailtxt=_("Email")),
