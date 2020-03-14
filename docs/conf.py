71487d6111558e5ce6849a4dcf7b253f09610dbd
henrinie <henri.nieminen@gmail.com>
2020-03-14 18:15:03 +0200

    Set version 1.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -26,9 +26,9 @@ copyright = '2018, Henri Nieminen'
 author = 'Henri Nieminen'
 
 # The short X.Y version
-version = '1.0.0'
+version = '1.0.1'
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
