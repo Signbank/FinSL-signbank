a2239bb3c1b78703467489d23d018bd56d8436c9
henrinie <henri.nieminen@gmail.com>
2024-09-01 13:13:16 +0000

    comment

--- signbank/video/models.py
+++ signbank/video/models.py
@@ -18,6 +18,7 @@ except ImportError:
 class GlossVideoStorage(SignbankStorage):
     """Video storage, handles saving to directories based on filenames first two characters."""
 
+    # Used for S3Storage
     location = "media"
 
     def __init__(self, *args, **kwargs):
